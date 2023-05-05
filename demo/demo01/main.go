7a91be207b9058b829582da8df33e4944b5cc217
guhuan769 <769540542@qq.com>
2023-05-05 14:59:28 +0800

    Go

--- demo/demo01/main.go
+++ demo/demo01/main.go
@@ -1,5 +1,8 @@
 package main
 
+import hello "HellowGolang/pakage"
+import hello2 "HellowGolang/pakage/package2"
+
 // 程序入口 func 声明
 func main() {
 	/*
@@ -7,9 +10,10 @@ func main() {
 		var str1 string
 		var num int
 		str1, num = f1("hellogolang f1")
-		:= 给变量赋值得同时去声明变量z
+		:= 给变量赋值得同时去声明变量
 		string1, num1 := f1("hello golang string1 - 1 f1")
 		fmt.Println(str1, num)
 		fmt.Println(string1, num1)*/
-
+	hello.F1()
+	hello2.F2()
 }
